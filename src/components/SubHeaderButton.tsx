63209e7094d6d626d3652dba5f398616b528dbe6
Kalzumaki <lumantasvankirk@gmail.com>
2025-05-22 20:39:25 +0800

    off buttons goods

--- src/components/SubHeaderButton.tsx
+++ src/components/SubHeaderButton.tsx
@@ -4,7 +4,7 @@ import UsersGenerateQRModal from "./UsersGenerateQrModal";
 
 export default function SubHeaderButton() {
   return (
-    <div className="flex justify-between items-center  py-4">
+ <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 py-4">
       <AddOfficerModal />
       <ScheduleMeeting />
       <UsersGenerateQRModal />
